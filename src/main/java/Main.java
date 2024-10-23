a7a9fb8b2bd6877d6d85b44d358213599acb794f
Andrew <andreyyes05@gmail.com>
2024-10-23 19:55:54 +0500

    Changed for's init value

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 99; i > 0; i--) {
+        for (int i = 9; i > 0; i--) {
             System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer!");
             System.out.println("Take one down, pass it around");
             System.out.println((i - 1) + " bottles of beer on the wall!\n");
